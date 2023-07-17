41117b509b785cbb2fa5311994ecb8fea4f89934
Micha-77 <133157755+Micha-77@users.noreply.github.com>
2023-07-18 02:29:00 +0300

    project finished

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ function getComputerChoice(min, max) {
 function winner() {
     let playerSelection = prompt("Choose: Rock, Paper, or Scissors?", "Rock, Paper, or Scissors")
     let computerSelection = getComputerChoice(0, options.length -1)
+    alert(`computer chose ${computerSelection}`)
+
     if (playerSelection === "Rock" && computerSelection === "Paper") {
         return "You Lose! Paper beats Rock";
 
@@ -26,11 +28,45 @@ function winner() {
 
     } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
         return "You Lose! Scissors beats Paper";
+
+    } else if (playerSelection === "Paper" && computerSelection === "Paper") {
+        return "Draw!";
+    } else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
+        return "Draw!";
+    } else if (playerSelection === "Rock" && computerSelection === "Rock") {
+        return "Draw!";
     }
 
 }
 
 
 
+function game() {
+    
+    let player = 0;
+    let computer = 0;
+    for (let i = 0; i < 5; i++) {
 
-winner("Rock")
+        let theWinner = winner();
+        alert(theWinner);
+        
+        if (theWinner === "You Lose! Paper beats Rock" || theWinner === "You Lose! Rock beats Scissors" || theWinner === "You Lose! Scissors beats Paper") {
+            computer++;
+            alert(`computer has ${computer} points`)
+        } else if (theWinner === "You Win! Rock beats Scissors" || theWinner === "You Win! Scissors beats Paper" || theWinner === "You Win! Paper beats Rock") {
+            player++;
+            alert(`player has ${player} points`)
+        } else if (theWinner === "Draw!") {
+            alert("It was a Draw so no points are added!")
+        }
+        
+    }
+    if (player > computer) {
+        alert(`Congratulations you win with ${player} points`)
+    } else if (player < computer){
+        alert(`You Lose, the score is (${computer} - ${player}) to computer, better luck next time.`)
+    } else if (player = computer) {
+        alert("This can't be, there must be a problem here!")
+    }
+}
+
